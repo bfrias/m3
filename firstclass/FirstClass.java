da5fc78260bb7c4a43435dff797cd652098ce1ae
bfrias <784460@seq.org>
2016-08-25 08:51:46 -0700

    Add files via upload

---
+++ firstclass/FirstClass.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class FirstClass here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FirstClass
+{
+    // instance variables - replace the example below with your own
+    private int a;
+    private int b;
+
+    /**
+     * Constructor for objects of class FirstClass
+     */
+    public FirstClass()
+    {
+        // initialise instance variables
+        a = 2;
+        b = 5;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int addEmUp()
+    {
+        // put your code here
+        return a + b;
+    }
+    
+    public int difference(){
+        return a - b;
+    }
+}
---
+++ firstclass/FirstClassRunner.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class FirstClassRunner here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FirstClassRunner
+
+{
+    public static void main (String[]argus){
+       FirstClass fc = new FirstClass();
+       System.out.print(fc.addEmUp());
+       System.out.print(" ' ");
+       System.out.println(fc.difference());
+    }
+}
