71e07815e02ebcff5ef12c6743746846d02f5bd0
bfrias <784460@seq.org>
2016-09-14 20:21:52 -0700

    Add files via upload

---
+++ lab4/LoopOne.java
@@ -0,0 +1,37 @@
+
+/**
+Bradley Frias
+ */
+public class LoopOne
+{
+    // instance variables - replace the example below with your own
+    
+
+    /**
+     * Constructor for objects of class LoopOne
+     */
+    public int[] loadArray()
+    {
+       
+        int[] Intarray = new int [10];
+        for (int i=0; i < 10; i++){
+          Intarray[i] = 5; 
+        }
+           
+        return Intarray;
+        
+    }
+    public void printArray(){
+        for (int i=0; i < 10; i++){
+        System.print.out( Intarray[i]);
+    }
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+   
+}
