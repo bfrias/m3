4f1d9fc628b84235559edc7e2fb61142b6da2d8c
bfrias <784460@seq.org>
2016-10-06 17:54:12 -0700

    Add files via upload

---
+++ act4/act4.java
@@ -0,0 +1,50 @@
+
+/**
+ * Write a description of class act4 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class act4
+{public int getLargest(int[][] nums){
+        for( int i = 0; i< nums.length; i++){
+            for (int j =0; j < nums[0].length; j++){
+                if (nums[i][j] <= nums[i][j]){
+                    count = nums[i][j];
+
+                }
+            }
+
+        }
+        return count;
+    }
+
+    public int getCount(int[][] nums){
+        for( int i = 0; i< nums.length; i++){
+            for (int j =0; j < nums[0].length; j++){
+                if (nums[i][j] <= 0]){
+                    count++;
+
+                }
+            }
+
+        }
+        return count;
+    }
+int x = 0
+ public int getColTotal(int[][] nums){
+        for( int i = 0; i< nums.length; i++){
+            for (int j =0; j < nums[i].length; j++){
+                if (nums[i][j] <= nums[i][j]){
+                    count + nums[i][j]
+                    count = nums[i][j];
+                    
+
+                }
+            }
+
+        }
+        return count;
+    }
+
+}
